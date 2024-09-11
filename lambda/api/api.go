cc85434b1dfe8b2a214d015ef1b15b5121cadb28
developertomek <tomekcodes@gmail.com>
2024-09-11 18:23:03 +0200

    new commit

--- lambda/api/api.go
+++ lambda/api/api.go
@@ -1,6 +1,10 @@
 package api
 
-import "lambda-func/database"
+import (
+	"fmt"
+	"lambda-func/database"
+	"lambda-func/types"
+)
 
 type ApiHandler struct {
 	dbStore database.DynamoDBClient
@@ -10,4 +14,26 @@ func NewApiHandler(dbStore database.DynamoDBClient) ApiHandler {
 	return ApiHandler{
 		dbStore: dbStore,
 	}
+}
+
+func (api ApiHandler) RegisterUserHandler(event types.RegisterUser) error {
+	if event.Username == "" || event.Password == "" {
+		return fmt.Errorf("request has empty parameters")
+	}
+
+	userExists, err := api.dbStore.DoesUserExist(event.Username)
+	if err != nil {
+		return fmt.Errorf("therean error checking if user exists %w", err)
+	}
+
+	if userExists {
+		return fmt.Errorf("a user with that username already exists")
+	}
+
+	err = api.dbStore.InsertUser(event)
+	if err != nil {
+		return fmt.Errorf("error registering the user %w", err)
+	}
+
+	return nil
 }
